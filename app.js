d7c3e6bf2d7b9ef811b81188ea87805f55f81419
rabiulislam007 <rabiulislam9@gmail.com>
2023-07-13 23:14:48 +0600

    middle added

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
 const fs = require('fs');
 const express = require('express');
-const { log } = require('console');
+const morgan = require('morgan');
 
 const app = express();
 
+// 1) Middlewares
+app.use(morgan('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the Middleware!!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+// 2) Route Handlers
+
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -100,6 +117,8 @@ const deleteTour = (req, res) => {
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
 
+// 3) Routes
+
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
 app
@@ -107,6 +126,8 @@ app
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+// 4) Start Server
 
 const port = 3000;
 app.listen(port, () => {
