cbae1824a406e098de78bc0a6359d669c816df09
rabiulislam007 <rabiulislam9@gmail.com>
2023-07-27 01:11:50 +0600

    error handling is done

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -13,11 +15,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
-app.use((req, res, next) => {
-  console.log('Hello from the Middleware!!');
-  next();
-});
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -28,5 +26,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 400));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Uncaught exceptions
+
+process.on('uncaughtException', (err) => {
+  console.log('uncaught Exception! SHUTTING DOWN');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
@@ -19,6 +27,15 @@ mongoose
   .then(() => console.log('DB Connection successful!'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}...`);
+});
+
+// Database password error
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! SHUTTING DOWN');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
