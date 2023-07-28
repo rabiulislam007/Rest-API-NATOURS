c80efedb885906a87f56261004c734372e610fe1
rabiulislam007 <rabiulislam9@gmail.com>
2023-07-29 01:46:28 +0600

    route protected feature is added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
@@ -59,22 +60,35 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
-    console.log(token);
+  }
 
-    if (!token) {
-      return next(
-        new AppError(
-          'You are not logged in! Please log in to get access.',
-          401,
-        ),
-      );
-    }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401),
+    );
   }
 
   // 2) Verification token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.id);
+  if (!currentUser) {
+    return next(
+      new AppError(
+        'The user belonging to the token does no longer exists',
+        401,
+      ),
+    );
+  }
 
   // 4) Check if user changed password aftet the token was issued
+  if (currentUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppError('User recently changed password! Please log in again.', 401),
+    );
+  }
+  // GRANT access to protected Route
+  req.user = currentUser;
   next();
 });
