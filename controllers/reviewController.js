47c82a9eb668fce1fff58e7ec5641a66dba9b868
rabiulislam007 <rabiulislam9@gmail.com>
2023-08-04 01:18:26 +0600

    adjust some missing authentication and authorization

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,20 +1,8 @@
 const Review = require('./../models/reviewModel');
-const catchAsync = require('./../utils/catchAsync');
 const factory = require('./handlerFactory');
+// const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const reviews = await Review.find(filter);
-
-  res.status(200).json({
-    status: 'success',
-    results: reviews.length,
-    data: {
-      reviews,
-    },
-  });
-});
+exports.getAllReviews = factory.getAll(Review);
 
 exports.setTourUserIds = (req, res, next) => {
   // allow nested routes
@@ -23,6 +11,7 @@ exports.setTourUserIds = (req, res, next) => {
   next();
 };
 
+exports.getReview = factory.getOne(Review);
 exports.createReview = factory.createOne(Review);
 exports.updateReview = factory.updateOne(Review);
 exports.deleteReview = factory.deleteOne(Review);
--- controllers/userController.js
+++ controllers/userController.js
@@ -11,18 +11,10 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.getAllUsers = catchAsync(async (req, res, next) => {
-  const users = await User.find();
-
-  // SEND RESPONSE
-  res.status(200).json({
-    status: 'success',
-    results: users.length,
-    data: {
-      users,
-    },
-  });
-});
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
 
 // Update only authenticated user, user can upadte it
 exports.updateMe = catchAsync(async (req, res, next) => {
@@ -60,19 +52,15 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getUser = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not defined',
-  });
-};
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
-    message: 'This route is not defined',
+    message: 'This route is not defined ! Please use /signup instead',
   });
 };
 
+exports.getUser = factory.getOne(User);
+exports.getAllUsers = factory.getAll(User);
 // Do not update password with this
 exports.updateUser = factory.updateOne(User);
 exports.deleteUser = factory.deleteOne(User);
